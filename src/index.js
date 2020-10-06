fb498ac20cdf9546f52ff2efacace885e0515790
Kuzmenko Alexandr <akuzmenko@vodafone.ua>
2020-10-06 13:36:40 +0300

    first commit

--- src/index.js
+++ src/index.js
@@ -1,1 +1,60 @@
-import './styles.css'
+import './styles.css';
+import thunk from "redux-thunk";
+import logger from "redux-logger";
+import { createStore, applyMiddleware, compose } from "redux";
+import { composeWithDevTools } from 'redux-devtools-extension';
+import { rootReducer } from "./redux/rootReducer";
+import { increment, decrement, asyncIncrement, changeTheme } from "./redux/actions";
+
+
+const counter = document.getElementById("counter");
+const addBtn = document.getElementById("add");
+const subBtn = document.getElementById("sub");
+const asyncBtn = document.getElementById("async");
+const themeBtn = document.getElementById("theme");
+
+// const store = createStore(
+//     rootReducer,
+//     compose(
+//         applyMiddleware(thunk, logger),
+//         window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+//         )
+// );
+
+const store = createStore(
+    rootReducer,
+    composeWithDevTools(
+        applyMiddleware(thunk, logger)
+    )
+);
+
+addBtn.addEventListener("click", () => {
+    store.dispatch(increment());
+});
+
+subBtn.addEventListener("click", () => {
+    store.dispatch(decrement());
+});
+
+asyncBtn.addEventListener("click", () => {
+    store.dispatch(asyncIncrement());
+    console.log("===", "asyncBtn.addEventListener")
+});
+
+themeBtn.addEventListener("click", () => {
+    const newTheme = document.body.classList.contains("light") ?
+        "dark" :
+        "light";
+    store.dispatch(changeTheme(newTheme));
+});
+
+store.subscribe(() => {
+    const state = store.getState();
+    counter.textContent = state.counter;
+    document.body.className = state.theme.value;
+    [addBtn, subBtn, themeBtn, asyncBtn].forEach(btn => {
+        btn.disabled = state.theme.disabled;
+    });
+});
+
+store.dispatch({ type: "INIT_APPLICATION" });
